09a528a9ecf6913c919be6a64942f937b54d8b38
matthew-alegrado <114251750+matthew-alegrado@users.noreply.github.com>
2024-11-07 20:40:57 -0800

    Update week1_exercise1.cpp

--- week1_exercise1.cpp
+++ week1_exercise1.cpp
@@ -1,14 +1,17 @@
-//STARTER 
-//THIS EXERCISE IS TO BE DONE IN THE ARM MBED SIMULATOR
-//http://195.130.59.221/
-//IF YOU HAVE ISSUES WITH THE SIMULATOR, CONTACT EMBEDDED LEAD
 #include "mbed.h"
 
-int main() {
-    while (1) {
-        printf("Week 1 Exercise 1");
+DigitalOut output(LED1);
+DigitalIn input(BUTTON1);
 
-        // MAKE SURE THERE IS ALWAYS A WAIT ON THE SIM OR IT WILL CRASH
-        wait_ms(500); 
+int main() {
+    while(1) {
+        int x = input; 
+        if (x) {
+            output = 1;
+        }
+        else {
+            output = 0;
+        }
+        wait_ms(50);
     }
 }
